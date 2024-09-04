b54f99c5d99620a8c2457ecfb69efeb4bad7f522
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-04 22:36:31 +0900

    Bumps node modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,10 @@ import eslint from '@eslint/js';
 import nextPlugin from '@next/eslint-plugin-next';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
-import importPlugin from 'eslint-plugin-import';
+import stylistic from '@stylistic/eslint-plugin';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import stylisticJsx from '@stylistic/eslint-plugin-jsx';
 
-import prettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 import { FlatCompat } from '@eslint/eslintrc';
 
@@ -26,11 +27,9 @@ export default tseslint.config(
       '.storybook',
     ],
   },
-  {
-    files: ['src/**/*.{jsx,ts,tsx}'],
-  },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   ...compat.config({
     extends: ['plugin:storybook/recommended'],
     ignorePatterns: ['!.storybook', 'storybook-static'],
@@ -38,7 +37,11 @@ export default tseslint.config(
   {
     files: ['src/**/*.{jsx,tsx}'],
     plugins: {
-      ['jsx-a11y']: jsxA11yPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      '@next/next': nextPlugin,
+      '@stylistic': stylistic,
+      '@stylistic/ts': stylisticTs,
+      '@stylistic/jsx': stylisticJsx,
     },
     extends: [
       ...compat.config(reactHooksPlugin.configs.recommended),
@@ -57,54 +60,20 @@ export default tseslint.config(
         typescript: {},
       },
     },
-  },
-  {
-    files: ['src/**/*.{ts,tsx}'],
-    plugins: {
-      import: importPlugin,
-    },
-    extends: [
-      ...tseslint.configs.recommended,
-      ...compat.config(importPlugin.configs.recommended),
-      ...compat.config(importPlugin.configs.typescript),
-    ],
-    settings: {
-      'import/internal-regex': '^~/',
-      'import/resolver': {
-        node: {
-          extensions: ['.ts', '.tsx'],
-        },
-        typescript: {
-          alwaysTryTypes: true,
-        },
-      },
-    },
-  },
-  {
-    files: ['src/**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      '@next/next': nextPlugin,
-    },
+    // @ts-ignore
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
       '@next/next/no-duplicate-head': 'off',
       '@next/next/no-img-element': 'error',
       '@next/next/no-page-custom-font': 'off',
-    },
-  },
-  {
-    files: ['src/**/*.{js,jsx,ts,tsx}'],
-    rules: {
-      ...prettier.rules,
-    },
-  },
-  {
-    rules: {
-      'react/display-name': 'off',
-      'import/namespace': 'off',
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
+      '@stylistic/semi': 'error',
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/jsx/jsx-indent': ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-parens': ['error', 'always'],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
     },
   },
 );
